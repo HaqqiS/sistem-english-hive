95fa5e5a6eea388f23d25a5be3a4234d47954bdd
Haqqi Sukmara Ishfahani <haqqi30sukmara@gmail.com>
2025-12-16 19:11:47 +0800

    chore: added waiting_list to statusMurid & add 100 in pagination

--- src/app/_components/shared/data-table.tsx
+++ src/app/_components/shared/data-table.tsx
@@ -268,7 +268,7 @@ export function DataTable<TData, TValue>({
 								/>
 							</SelectTrigger>
 							<SelectContent side="top">
-								{[5, 10, 20, 30, 40, 50].map((pageSize) => (
+								{[10, 20, 30, 40, 50, 100].map((pageSize) => (
 									<SelectItem key={pageSize} value={`${pageSize}`}>
 										{pageSize}
 									</SelectItem>

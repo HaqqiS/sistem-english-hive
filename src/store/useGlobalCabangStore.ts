11845acc89074c503e06fe17f28028c7ed16f7e9
Haqqi Sukmara Ishfahani <haqqi30sukmara@gmail.com>
2026-01-09 19:08:24 +0800

    chore: save cabangId state to cookies

--- src/store/useGlobalCabangStore.ts
+++ src/store/useGlobalCabangStore.ts
@@ -1,11 +1,31 @@
+import Cookies from "js-cookie";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface GlobalCabangState {
 	activeCabangId: string;
 	setActiveCabangId: (cabangId: string) => void;
 }
 
-export const useGlobalCabangStore = create<GlobalCabangState>((set) => ({
-	activeCabangId: "ALL",
-	setActiveCabangId: (cabangId) => set({ activeCabangId: cabangId }),
-}));
+export const useGlobalCabangStore = create<GlobalCabangState>()(
+	persist(
+		(set) => ({
+			activeCabangId: "ALL",
+			setActiveCabangId: (cabangId) => set({ activeCabangId: cabangId }),
+		}),
+		{
+			name: "cabang_storage",
+			storage: createJSONStorage(() => ({
+				getItem: (name) => {
+					return Cookies.get(name) || null;
+				},
+				setItem: (name, value) => {
+					Cookies.set(name, value, { expires: 7, path: "/" });
+				},
+				removeItem: (name) => {
+					Cookies.remove(name);
+				},
+			})),
+		},
+	),
+);
